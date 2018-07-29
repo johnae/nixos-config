766f2a7edec338d5d0601d91c91062635236decf
John Axel Eriksson <john@insane.se>
2018-07-29 15:11:48 +0200

    use self/super properly
    Former-commit-id: 4954e6e4d8c72940e0b28db30437a8b5a7e5d6cd
    Former-commit-id: 5bbc0061cff71f4086226a20c9f64c5b239831e6

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -1,9 +1,9 @@
 self: super: {
-  spook = self.callPackage ../packages/spook { };
-  system-san-francisco-font = self.callPackage ../packages/system-san-francisco-font { };
-  san-francisco-mono-font = self.callPackage ../packages/san-francisco-mono-font { };
-  alacritty = self.callPackage ../packages/alacritty { };
-  fire = self.callPackage ../packages/fire { };
-  btr-snap = self.callPackage ../packages/btr-snap { };
-  notify-osd = self.callPackage ../packages/notify-osd-customizable { };
+  spook = super.callPackage ../packages/spook { };
+  system-san-francisco-font = super.callPackage ../packages/system-san-francisco-font { };
+  san-francisco-mono-font = super.callPackage ../packages/san-francisco-mono-font { };
+  alacritty = super.callPackage ../packages/alacritty { };
+  fire = super.callPackage ../packages/fire { };
+  btr-snap = super.callPackage ../packages/btr-snap { };
+  notify-osd = super.callPackage ../packages/notify-osd-customizable { };
 }
