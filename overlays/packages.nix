aabb678a8db13cb2a436e8ed0ab523a4f545a67a
John Axel Eriksson <john@insane.se>
2018-10-15 11:32:50 +0200

    Remove unused notify-osd (on wayland now)
    Former-commit-id: 952b9dd5dce6c533d704daad47e36c1563cca044
    Former-commit-id: 512f1f1ce4d18f2a617b9c658ac2d94dec91218c

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -2,7 +2,6 @@ self: super: {
   system-san-francisco-font = super.callPackage ../packages/system-san-francisco-font { };
   san-francisco-mono-font = super.callPackage ../packages/san-francisco-mono-font { };
   btr-snap = super.callPackage ../packages/btr-snap { };
-  notify-osd = super.callPackage ../packages/notify-osd-customizable { };
   redshiftwl = with super.pkgs; super.callPackage ../packages/redshift {
     inherit (python3Packages) python pygobject3 pyxdg wrapPython;
     inherit (darwin.apple_sdk.frameworks) CoreLocation ApplicationServices Foundation Cocoa;
