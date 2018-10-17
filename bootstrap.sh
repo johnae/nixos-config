852bb27d3ede3af84af4659adba354f122ac49a8
John Axel Eriksson <john@insane.se>
2018-10-17 16:29:03 +0200

    Try to keep to 80 col limit.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,16 +5,19 @@ DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 ## This script bootstraps a nixos install. The assumptions are:
 # 1. You want an EFI System Partition (500MB) - so no BIOS support
 # 2. You want encrypted root and swap
-# 3. You want swap space size to be half of RAM as per modern standards (eg. see https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/installation_guide/sect-disk-partitioning-setup-x86#sect-recommended-partitioning-scheme-x86)
-# 4. You want to use btrfs for everything else and you want to use subvolumes for /, /var and /home
-# 5. You want to not care about atime and you want to compress your fs using zstd
+# 3. You want swap space size to be half of RAM as per modern standards
+# 4. You want to use btrfs for everything else and you want to use subvolumes
+#    for /, /var and /home
+# 5. You want to not care about atime and you want
+#    to compress your fs using zstd
 
 # set -x
 
-## Generally this should really be /dev/random as that will be cryptographically of high quality,
-## however for testing purposes I allow it to be overridden. Just don't do this unless you have a
-## good reason. For example, in a VM when testing the install script, it may be beneficial to use urandom
-## instead as it will likely generate entropy properly as opposed to random.
+## Generally this should really be /dev/random as that will be cryptographically
+## of high quality, however for testing purposes I allow it to be overridden.
+## Just don't do this unless you have a good reason. For example, in a VM when
+## testing the install script, it may be beneficial to use urandom instead as
+## it will likely generate entropy properly as opposed to random.
 
 DEVRANDOM=${DEVRANDOM:-/dev/random}
 
@@ -56,8 +59,8 @@ sgdisk -Z $DISK
 efi_space=500M # EF00 EFI Partition
 luks_key_space=3M # 8300
 # set to half amount of RAM
-swap_space=$(($(free --giga | tail -n+2 | head -1 | awk '{print $2}') / 2))G # 8300
-# special case when there's very little ram - perhaps this should be dealt with differently?
+swap_space=$(($(free --giga | tail -n+2 | head -1 | awk '{print $2}') / 2))G
+# special case when there's very little ram
 if [ "$swap_space" = "0G" ]; then
     swap_space="1G"
 fi
@@ -112,13 +115,16 @@ mkfs.btrfs -L root /dev/mapper/cryptroot
 # and create the efi boot partition
 mkfs.vfat $DISK_EFI
 
-DECRYPTED_SWAP_UUID=$(ls -lah /dev/disk/by-uuid/ | grep $DM_SWAP | awk '{print $9}')
+DECRYPTED_SWAP_UUID=$(ls -lah /dev/disk/by-uuid/ | \
+                          grep $DM_SWAP | awk '{print $9}')
 # enable swap on the decrypted cryptswap
 swapon /dev/disk/by-uuid/$DECRYPTED_SWAP_UUID
 
-DECRYPTED_ROOT_UUID=$(ls -lah /dev/disk/by-uuid/ | grep $DM_ROOT | awk '{print $9}')
+DECRYPTED_ROOT_UUID=$(ls -lah /dev/disk/by-uuid/ | \
+                          grep $DM_ROOT | awk '{print $9}')
 # mount the decrypted cryptroot to /mnt (btrfs)
-mount -o rw,noatime,compress=zstd,ssd,space_cache /dev/disk/by-uuid/$DECRYPTED_ROOT_UUID /mnt
+mount -o rw,noatime,compress=zstd,ssd,space_cache \
+      /dev/disk/by-uuid/$DECRYPTED_ROOT_UUID /mnt
 
 # now create btrfs subvolumes we're interested in having
 cd /mnt
@@ -131,22 +137,30 @@ cd $DIR
 umount /mnt
 
 # mount the "root" (@) subvolume to /mnt
-mount -o rw,noatime,compress=zstd,ssd,space_cache,subvol=@ /dev/disk/by-uuid/$DECRYPTED_ROOT_UUID /mnt
+mount -o rw,noatime,compress=zstd,ssd,space_cache,subvol=@ \
+      /dev/disk/by-uuid/$DECRYPTED_ROOT_UUID /mnt
 # mount @home subvolume to /mnt/home
-mount -o rw,noatime,compress=zstd,ssd,space_cache,subvol=@home /dev/disk/by-uuid/$DECRYPTED_ROOT_UUID /mnt/home
+mount -o rw,noatime,compress=zstd,ssd,space_cache,subvol=@home \
+      /dev/disk/by-uuid/$DECRYPTED_ROOT_UUID /mnt/home
 # mount @var subvolume to /mnt/var
-mount -o rw,noatime,compress=zstd,ssd,space_cache,subvol=@var /dev/disk/by-uuid/$DECRYPTED_ROOT_UUID /mnt/var
+mount -o rw,noatime,compress=zstd,ssd,space_cache,subvol=@var \
+      /dev/disk/by-uuid/$DECRYPTED_ROOT_UUID /mnt/var
 
-BOOT_UUID=$(ls -lah /dev/disk/by-uuid/ | grep $(basename $DISK_EFI) | awk '{print $9}')
+BOOT_UUID=$(ls -lah /dev/disk/by-uuid/ | \
+                grep $(basename $DISK_EFI) | awk '{print $9}')
 # and mount the boot partition
 mount /dev/disk/by-uuid/$BOOT_UUID /mnt/boot
 
 nixos-generate-config --root /mnt
-cat /mnt/etc/nixos/hardware-configuration.nix | grep -v "boot\.initrd\.luks" | grep -v "^}\$" > /mnt/etc/nixos/hardware-configuration.nix_tmp
+cat /mnt/etc/nixos/hardware-configuration.nix | grep -v "boot\.initrd\.luks" | \
+    grep -v "^}\$" > /mnt/etc/nixos/hardware-configuration.nix_tmp
 
-CRYPTKEY_UUID=$(ls -lah /dev/disk/by-uuid/ | grep $(basename $DISK_CRYPTKEY) | awk '{print $9}')
-ROOT_UUID=$(ls -lah /dev/disk/by-uuid/ | grep $(basename $DISK_ROOT) | awk '{print $9}')
-SWAP_UUID=$(ls -lah /dev/disk/by-uuid/ | grep $(basename $DISK_SWAP) | awk '{print $9}')
+CRYPTKEY_UUID=$(ls -lah /dev/disk/by-uuid/ | \
+                    grep $(basename $DISK_CRYPTKEY) | awk '{print $9}')
+ROOT_UUID=$(ls -lah /dev/disk/by-uuid/ | \
+                grep $(basename $DISK_ROOT) | awk '{print $9}')
+SWAP_UUID=$(ls -lah /dev/disk/by-uuid/ | \
+                grep $(basename $DISK_SWAP) | awk '{print $9}')
 
 cat <<EOF>> /mnt/etc/nixos/hardware-configuration.nix_tmp
   boot.initrd.luks.devices = {
@@ -167,9 +181,13 @@ cat <<EOF>> /mnt/etc/nixos/hardware-configuration.nix_tmp
 }
 EOF
 
-diff /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/hardware-configuration.nix_tmp
+diff /mnt/etc/nixos/hardware-configuration.nix \
+     /mnt/etc/nixos/hardware-configuration.nix_tmp
+
 rm /mnt/etc/nixos/hardware-configuration.nix
-mv /mnt/etc/nixos/hardware-configuration.nix_tmp /mnt/etc/nixos/hardware-configuration.nix
+
+mv /mnt/etc/nixos/hardware-configuration.nix_tmp \
+   /mnt/etc/nixos/hardware-configuration.nix
 
 echo "Installing mkpasswd"
 nix-env -i mkpasswd
