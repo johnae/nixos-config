3cca5bd5149d090fa230780e448b1b418769c193
John Axel Eriksson <john@insane.se>
2019-01-21 22:54:01 +0100

    Add gpg agent env vars

--- configuration.nix
+++ configuration.nix
@@ -78,6 +78,8 @@ in
 
   environment.shellInit = ''
     gpg-connect-agent /bye
+    export GPG_TTY=$(tty)
+    export GPG_AGENT_INFO=$(gpgconf --list-dirs agent-socket)
     export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
   '';
 
