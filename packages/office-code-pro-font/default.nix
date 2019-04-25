45bf003fa6d7ab7367f42b13aec342e65858ead7
John Axel Eriksson <john@insane.se>
2019-04-25 23:03:00 +0200

    Add patched office-code-pro font

--- packages/office-code-pro-font/default.nix
+++ packages/office-code-pro-font/default.nix
@@ -1,50 +1,50 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  version = "2018-12-23";
+  version = "2019-04-23";
   name = "font-office-code-pro-${version}";
-  at = "ce12a41d6070e545cf2293b2c6fa7c6ef367f29d";
+  at = "fcf415b0635765a59085f93ee2186a53adfecac4";
 
   srcs = [
     (fetchurl {
-      url = "https://raw.githubusercontent.com/nathco/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D/OTF/OfficeCodeProD-Bold.otf";
+      url = "https://raw.githubusercontent.com/johnae/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D%20Patched/OfficeCodeProD-Bold.otf";
       name = "office-code-pro-d-bold.otf";
-      sha256 = "0lycfsh1ijchknwl550a4r35l7s8mm4nqbq1fw538xjyhzxrw34n";
+      sha256 = "0pc16fyslqcy7p1lm8x244w4v1ivfl6zvskc2gc4q81qrgvls8yh";
     })
     (fetchurl {
-      url = "https://raw.githubusercontent.com/nathco/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D/OTF/OfficeCodeProD-BoldItalic.otf";
+      url = "https://raw.githubusercontent.com/johnae/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D%20Patched/OfficeCodeProD-BoldItalic.otf";
       name = "office-code-pro-d-bold-italic.otf";
-      sha256 = "1sk2645cdwhm3z3shgsnhb6iiz0dsx861d1l3rg323w7bmlf4r5i";
+      sha256 = "00afr9ka6z1mgly03y13bzb5adwcxa4zk56c16aqcv4kc7r6w4qg";
     })
     (fetchurl {
-      url = "https://raw.githubusercontent.com/nathco/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D/OTF/OfficeCodeProD-Light.otf";
+      url = "https://raw.githubusercontent.com/johnae/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D%20Patched/OfficeCodeProD-Light.otf";
       name = "office-code-pro-d-light.otf";
-      sha256 = "139bb2dvg0xgga48dkxav5adwrm2asxsybbzwg08102r5yaam3rb";
+      sha256 = "06w21sbpfyccv2fd0i004kghvn1y79amy0r0gi4lsjzyvxcsf2wr";
     })
     (fetchurl {
-      url = "https://raw.githubusercontent.com/nathco/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D/OTF/OfficeCodeProD-LightItalic.otf";
+      url = "https://raw.githubusercontent.com/johnae/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D%20Patched/OfficeCodeProD-LightItalic.otf";
       name = "office-code-pro-d-light-italic.otf";
-      sha256 = "04xi1x3v19ylxl4x8z1zyzc4wgbfqx500ly6g0cjgc7k44zfa8gl";
+      sha256 = "0a6lhcn4cg46qyr7cnx9czfcvczs58salgx58phacgjl7krpalvy";
     })
     (fetchurl {
-      url = "https://raw.githubusercontent.com/nathco/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D/OTF/OfficeCodeProD-Medium.otf";
+      url = "https://raw.githubusercontent.com/johnae/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D%20Patched/OfficeCodeProD-Medium.otf";
       name = "office-code-pro-d-medium.otf";
-      sha256 = "0q4s1n8ksdwsb0g3s7y8ki69wysj6jdncdnr451j2xjknqd6hk94";
+      sha256 = "045s156y94gj0gpkwn1rbkqnxz2c6lrk749z7kjj98x0r7q0llwd";
     })
     (fetchurl {
-      url = "https://raw.githubusercontent.com/nathco/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D/OTF/OfficeCodeProD-MediumItalic.otf";
+      url = "https://raw.githubusercontent.com/johnae/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D%20Patched/OfficeCodeProD-MediumItalic.otf";
       name = "office-code-pro-d-medium-italic.otf";
-      sha256 = "1bn98gb684ksvzpd4xxmcb1qyqz8xj1ghl1dx6ijxlwirykhgxdh";
+      sha256 = "1x0sdfxz8v0qfprmjw7794bf4ddl5jzkdfyflq2c3x79ygf8ckzm";
     })
     (fetchurl {
-      url = "https://raw.githubusercontent.com/nathco/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D/OTF/OfficeCodeProD-Regular.otf";
+      url = "https://raw.githubusercontent.com/johnae/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D%20Patched/OfficeCodeProD-Regular.otf";
       name = "office-code-pro-d-regular.otf";
-      sha256 = "01csxpav3mnnr1ciphqc4q7n9jy2zs7f9jj1mifr6k3ycxhrhdfq";
+      sha256 = "1m9c8dj5vj6198amsd161gg8bg8mvg7xd715x92lbk3pkm899nv8";
     })
     (fetchurl {
-      url = "https://raw.githubusercontent.com/nathco/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D/OTF/OfficeCodeProD-RegularItalic.otf";
+      url = "https://raw.githubusercontent.com/johnae/Office-Code-Pro/${at}/Fonts/Office%20Code%20Pro%20D%20Patched/OfficeCodeProD-RegularItalic.otf";
       name = "office-code-pro-d-regular-italic.otf";
-      sha256 = "1zysm0pl500bcwv6790ypi7jrwxcxwfyk8z9ksjq16yy831i1b7v";
+      sha256 = "0pq3ykfqckqckii5cy3mwlwa97mzs1bac4lgj6ln0pp5279ckcdx";
     })
   ];
 
@@ -65,11 +65,11 @@ stdenv.mkDerivation rec {
 
   outputHashAlgo = "sha256";
   outputHashMode = "recursive";
-  outputHash = "0cw8vn894sxc17mvi91ddgn3n64n3i3vrfrq8f9mq96qqbb34kfh";
+  outputHash = "0bvk1j094wxsgl01p8843sw4ys2r8rwg300ip4va4vcwbzr13ysr";
 
   meta = {
     description = "Office Code Pro Font (patched Source Code Pro Font)";
-    homepage = https://github.com/nathco/Office-Code-Pro;
+    homepage = https://github.com/johnae/Office-Code-Pro;
     license = stdenv.lib.licenses.sil;
     platforms = stdenv.lib.platforms.all;
     maintainers = [];
